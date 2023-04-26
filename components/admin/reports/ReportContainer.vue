e2db97d8acbd8c294494e05d6d1e1f795736c332
Jervin Macalawa <jervinmac@yahoo.com>
2023-04-26 21:40:01 +0800

    done

--- components/admin/reports/ReportContainer.vue
+++ components/admin/reports/ReportContainer.vue
@@ -2,14 +2,27 @@
   <div>
     <div class="pa-5 text-h5"><b>Report Management</b></div>
     <div>
+      <div>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-card class="pa-4">
+              <v-icon class="display-1"></v-icon>
+              <div class="title">No. of Bookings</div>
+              <div class="subtitle">{{total_bookings }}</div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" md="6">
+            <v-card class="pa-4">
+              <v-icon class="display-1"></v-icon>
+              <div class="title">No. of bookings today</div>
+              <div class="subtitle">{{ total_bookings_today }}</div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
       <div class="py-10">
         <div class="pa-5">
-          <VueApexCharts
-            :series="series"
-            :data="data"
-            :options="chartOptions"
-            height="300"
-          />
+          <VueApexCharts :series="series" :data="data" :options="chartOptions" height="300" />
         </div>
       </div>
     </div>
@@ -18,10 +31,48 @@
 
 <script>
 import VueApexCharts from "vue-apexcharts";
+import {mapState} from 'vuex';
 export default {
+  computed:{
+    ...mapState('book',['book_data']),
+    total_bookings(){
+      return this.book_data.length
+    },
+    total_bookings_today(){
+      const today = new Date().toISOString().slice(0, 10); 
+      const todayEntries = this.book_data.filter(entry => entry.created_date.slice(0, 10) === today);
+      alert(todayEntries.length)
+      return todayEntries.length
+    }
+  },
+  created(){
+    this.$store.dispatch('book/view')
+  },
   components: { VueApexCharts },
   data() {
     return {
+      cards: [
+        {
+          title: 'Number of Users',
+          subtitle: '1000',
+          icon: 'mdi-account-group'
+        },
+        {
+          title: 'Number of Bookings',
+          subtitle: this.book_data,
+          icon: 'mdi-calendar-multiple-check'
+        },
+        {
+          title: 'Number of Pending',
+          subtitle: '200',
+          icon: 'mdi-calendar-alert'
+        },
+        {
+          title: 'Number of Approved',
+          subtitle: '300',
+          icon: 'mdi-calendar-check'
+        }
+      ],
       carousel: 1,
       series: [
         {
@@ -108,5 +159,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,277 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-if="
-        $route.name != 'login' &&
-        $route.name != 'index' &&
-        $route.name != 'register' &&
-        $route.name != 'redeem' && 
-        $route.name != 'contact' 
-      "
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      color="secondary"
-      :clipped="clipped"
-      dark
-      fixed
-      app
-    >
+    <VueHtml2pdf :show-layout="false" :float-layout="true" :enable-download="true" :paginate-elements-by-height="1400"
+      filename="myPDF" :pdf-quality="2" :manual-pagination="false" pdf-format="a4" pdf-orientation="portrait"
+      pdf-content-width="800px" ref="html2Pdf">
+      <v-section slot="pdf-content">
+        <v-card class="pa-10" style="padding:70px">
+          <v-row>
+            <v-col>
+              <div>
+                Firstname
+              </div>
+              <v-text-field outlined v-model="register.firstname"></v-text-field>
+            </v-col>
+            <v-col>
+              Middlename
+              <v-text-field outlined v-model="register.middlename"></v-text-field>
+            </v-col>
+            <v-col>
+              Lastname
+              <v-text-field outlined v-model="register.lastname"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div>
+                Starting Weight
+              </div>
+              <v-text-field outlined v-model="register.height"></v-text-field>
+            </v-col>
+            <v-col>
+              Weight
+              <v-text-field outlined v-model="register.weight"></v-text-field>
+            </v-col>
+            <v-col>
+              Goal
+              <v-text-field outlined v-model="register.goal"></v-text-field>
+            </v-col>
+          </v-row>
+          <div class="pt-10">
+
+          </div>
+          <v-divider></v-divider>
+          <div class="pb-10">
+            Health Conditions
+          </div>
+          <v-row>
+            <v-col>
+              <div>
+                Heart Problems(Yes/No)
+              </div>
+              <v-text-field outlined v-model="register.hp"></v-text-field>
+            </v-col>
+            <v-col>
+              Blood Pressure problems(Yes/No)
+              <v-text-field outlined v-model="register.bp"></v-text-field>
+            </v-col>
+            <v-col>
+              Chest pain(Yes/No)
+              <v-text-field outlined v-model="register.cp"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div>
+                Asthma or Breathing difficulties(Yes/No)
+              </div>
+              <v-text-field outlined v-model="register.bd"></v-text-field>
+            </v-col>
+            <v-col>
+              Pregnant/Just gave birth(Yes/No)
+              <v-text-field outlined v-model="register.pregnant"></v-text-field>
+            </v-col>
+            <v-col>
+              Do you smoke(Yes/No)
+              <v-text-field outlined v-model="register.smoke"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <div>
+                Do you drink alcohol?(Yes/No)
+              </div>
+              <v-text-field outlined v-model="register.alcohol"></v-text-field>
+            </v-col>
+            <v-col>
+              Are you on any medication(Yes/No)
+              <v-text-field outlined v-model="register.medication"></v-text-field>
+            </v-col>
+          </v-row>
+          <div>
+            <div class="text-h4">
+              Liability Waiver
+            </div>
+            <div>
+              I, the undersigned, being aware of my own health and physical condition, and having knowledge that my
+              participation in any exercise program may be injurious to my health, I am voluntary participating in
+              physical
+              activity.
+            </div>
+            <div class="pt-10">
+              Having knowledge, I hereby acknowledge this release, any representatives, agents, and successors from
+              liability for accidental injury or illness which I may incur as a result of participating in the said
+              physical
+              activity. I hereby assume all risks connected therewith and consent to participate in said program.
+            </div>
+            <div>
+              participating in the said physical activity. I hereby assume all risks connected therewith and consent to
+              participate in said program.
+              I agree to disclose any physical limitations, disabilities, ailments, or impairments which may affect my
+              ability to participate in said fitness program. My membership is non-transferrable to another person or
+              another branch and is non-refundable.
+            </div>
+            <div>
+              <v-row>
+                <v-col>
+                  <div>
+                    Emergency Contact Person:
+                  </div>
+                  <v-text-field outlined v-model="register.emergency_contact"></v-text-field>
+                </v-col>
+                <v-col>
+                  Contact Number
+                  <v-text-field outlined v-model="register.contact_number"></v-text-field>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+          <div>
+            <!-- <v-btn @click="generatePDF">download waiver</v-btn> -->
+          </div>
+          <div style="padding-top:80px">
+            Signature Over Printed Name
+          </div>
+        </v-card>
+      </v-section>
+    </VueHtml2pdf>
+    <v-dialog v-model="waiveformModel" width="1200">
+      <div class="d-none">
+
+      </div>
+      <v-card class="pa-10">
+        <v-row>
+          <v-col>
+            <div>
+              Firstname
+            </div>
+            <v-text-field outlined v-model="register.firstname"></v-text-field>
+          </v-col>
+          <v-col>
+            Middlename
+            <v-text-field outlined v-model="register.middlename"></v-text-field>
+          </v-col>
+          <v-col>
+            Lastname
+            <v-text-field outlined v-model="register.lastname"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div>
+              Starting Weight
+            </div>
+            <v-text-field outlined v-model="register.height"></v-text-field>
+          </v-col>
+          <v-col>
+            Weight
+            <v-text-field outlined v-model="register.weight"></v-text-field>
+          </v-col>
+          <v-col>
+            Goal
+            <v-text-field outlined v-model="register.goal"></v-text-field>
+          </v-col>
+        </v-row>
+        <div class="pt-10">
+
+        </div>
+        <v-divider></v-divider>
+        <div class="pb-10">
+          Health Conditions
+        </div>
+        <v-row>
+          <v-col>
+            <div>
+              Heart Problems(Yes/No)
+            </div>
+            <v-text-field outlined v-model="register.hp"></v-text-field>
+          </v-col>
+          <v-col>
+            Blood Pressure problems(Yes/No)
+            <v-text-field outlined v-model="register.bp"></v-text-field>
+          </v-col>
+          <v-col>
+            Chest pain(Yes/No)
+            <v-text-field outlined v-model="register.cp"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div>
+              Asthma or Breathing difficulties(Yes/No)
+            </div>
+            <v-text-field outlined v-model="register.bd"></v-text-field>
+          </v-col>
+          <v-col>
+            Pregnant/Just gave birth(Yes/No)
+            <v-text-field outlined v-model="register.pregnant"></v-text-field>
+          </v-col>
+          <v-col>
+            Do you smoke(Yes/No)
+            <v-text-field outlined v-model="register.smoke"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <div>
+              Do you drink alcohol?(Yes/No)
+            </div>
+            <v-text-field outlined v-model="register.alcohol"></v-text-field>
+          </v-col>
+          <v-col>
+            Are you on any medication(Yes/No)
+            <v-text-field outlined v-model="register.medication"></v-text-field>
+          </v-col>
+        </v-row>
+        <div>
+          <div class="text-h4">
+            Liability Waiver
+          </div>
+          <div>
+            I, the undersigned, being aware of my own health and physical condition, and having knowledge that my
+            participation in any exercise program may be injurious to my health, I am voluntary participating in physical
+            activity.
+          </div>
+          <div class="pt-10">
+            Having knowledge, I hereby acknowledge this release, any representatives, agents, and successors from
+            liability for accidental injury or illness which I may incur as a result of participating in the said physical
+            activity. I hereby assume all risks connected therewith and consent to participate in said program.
+          </div>
+          <div>
+            participating in the said physical activity. I hereby assume all risks connected therewith and consent to
+            participate in said program.
+            I agree to disclose any physical limitations, disabilities, ailments, or impairments which may affect my
+            ability to participate in said fitness program. My membership is non-transferrable to another person or
+            another branch and is non-refundable.
+          </div>
+          <div>
+            <v-row>
+              <v-col>
+                <div>
+                  Emergency Contact Person:
+                </div>
+                <v-text-field outlined v-model="register.emergency_contact"></v-text-field>
+              </v-col>
+              <v-col>
+                Contact Number
+                <v-text-field outlined v-model="register.contact_number"></v-text-field>
+              </v-col>
+            </v-row>
+          </div>
+        </div>
+        <div>
+          <v-btn @click="generatePDF">download waiver</v-btn>
+        </div>
+      </v-card>
+    </v-dialog>
+    <v-navigation-drawer v-if="$route.name != 'login' &&
+      $route.name != 'index' &&
+      $route.name != 'register' &&
+      $route.name != 'redeem' &&
+      $route.name != 'contact'
+      " v-model="drawer" :mini-variant="miniVariant" color="secondary" :clipped="clipped" dark fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -51,71 +299,38 @@
       </v-btn>
     </v-app-bar>
     <!-- is not logged in yet -->
-    <v-app-bar
-      color="white"
-      v-if="
-        $route.name == 'login' ||
-        $route.name == 'index' ||
-        $route.name == 'redeem' ||
-        $route.name == 'register' ||
-        $route.name == 'contact'
-      "
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar color="white" v-if="$route.name == 'login' ||
+      $route.name == 'index' ||
+      $route.name == 'redeem' ||
+      $route.name == 'register' ||
+      $route.name == 'contact'
+      " :clipped-left="clipped" fixed app>
       <div align="start">
-        <v-img
-          class="pointer"
-          src="/images/PFP.jpeg"
-          height="60"
-          width="100"
-          contain
-          @click="goIndex"
-        />
+        <v-img class="pointer" src="/images/PFP.jpeg" height="60" width="100" contain @click="goIndex" />
       </div>
       <v-spacer />
-      <div
-        :class="
-          $route.name == 'contact'
-            ? 'px-10 pointer secondary--text'
-            : 'px-10 pointer'
-        "
-        @click="pushRoute('contact')"
-      >
+      <div :class="$route.name == 'contact'
+          ? 'px-10 pointer secondary--text'
+          : 'px-10 pointer'
+        " @click="pushRoute('contact')">
         Contact Us
       </div>
-      <div
-        :class="
-          $route.name == 'login'
-            ? 'px-10 pointer secondary--text'
-            : 'px-10 pointer'
-        "
-        @click="pushRoute('login')"
-        v-if="!$auth.loggedIn"
-      >
+      <div :class="$route.name == 'login'
+          ? 'px-10 pointer secondary--text'
+          : 'px-10 pointer'
+        " @click="pushRoute('login')" v-if="!$auth.loggedIn">
         Login
       </div>
-      <div
-        :class="
-          $route.name == 'login'
-            ? 'px-10 pointer secondary--text'
-            : 'px-10 pointer'
-        "
-        @click="waiverForm()"
-        v-if="!$auth.loggedIn"
-      >
+      <div :class="$route.name == 'login'
+          ? 'px-10 pointer secondary--text'
+          : 'px-10 pointer'
+        " @click="waiverForm()" v-if="!$auth.loggedIn">
         Waiver Form
       </div>
-      <div
-        :class="
-          $route.name == 'client-profile'
-            ? 'px-10 pointer secondary--text'
-            : 'px-10 pointer'
-        "
-        @click="pushRoute('client/profile')"
-        v-else
-      >
+      <div :class="$route.name == 'client-profile'
+          ? 'px-10 pointer secondary--text'
+          : 'px-10 pointer'
+        " @click="pushRoute('client/profile')" v-else>
         My Profile
       </div>
       <div class="px-10 pointer" v-if="$auth.loggedIn">
@@ -179,10 +394,16 @@
 </template>
 
 <script>
+import VueHtml2pdf from "vue-html2pdf";
 export default {
+  components: { VueHtml2pdf },
   methods: {
-    waiverForm(){
-      window.location="https://chatmind.s3.ap-northeast-1.amazonaws.com/F3-Membership-Form+(1).docx"
+    generatePDF() {
+      this.$refs.html2Pdf.generatePdf();
+    },
+    waiverForm() {
+      this.waiveformModel = true
+      // window.location="https://chatmind.s3.ap-northeast-1.amazonaws.com/F3-Membership-Form+(1).docx"
     },
     pushRoute(link) {
       window.location.href = `/${link}`;
@@ -194,6 +415,8 @@ export default {
   name: "DefaultLayout",
   data() {
     return {
+      register: {},
+      waiveformModel: false,
       clipped: false,
       drawer: true,
       fixed: false,
@@ -209,7 +432,7 @@ export default {
           title: "Dashboard",
           to: "/",
         },
-         {
+        {
           icon: "mdi-chart-bubble",
           title: "User Management",
           to: "/admin/users",
